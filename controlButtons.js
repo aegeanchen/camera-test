702c51c0cbda66ad12ebae807c1a24026db7dc85
Yijun Chen <yjchen1001@gmail.com>
2018-07-31 11:51:08 -0400

    731commit

--- controlButtons.js
+++ controlButtons.js
@@ -1,5 +1,10 @@
 var vid, playbtn, pictureBtn, pictureBox, cameraBtn, cameraBox;
 
+// var x = event.clientX;
+// var y = event.clientY;
+
+
+
 function initializePlayer() {
     vid = document.getElementById("video");
     playbtn = document.getElementById("playPauseBtn");
@@ -13,24 +18,47 @@ function initializePlayer() {
     cameraBtn.addEventListener("click", cameraSettings, false);
     cameraBox = document.getElementById("cameraSettings");
 
+    controlComponent = document.getElementById("videoControls");
+
 }
 window.onload = initializePlayer();
+pictureBox.style.display = "none";
+cameraBox.style.display = "none";
+
+
+
+window.addEventListener('mouseup', function (event) {
+    if (event.target != pictureBox && event.target.parentNode.parentNode != pictureBox && event.target.parentNode.parentNode.parentNode != pictureBox) {
+        pictureBox.style.display = "none";
+    }
+    if (event.target != cameraBox && event.target.parentNode.parentNode != cameraBox && event.target.parentNode.parentNode.parentNode != pictureBox) {
+        cameraBox.style.display = "none";
+    }
+
+});
+
+
+function mousein() {
+    controlComponent.style.visibility = 'inherit';
+}
+
+function mouseout() {
+    controlComponent.style.visibility = 'hidden';
+
+}
+
 
 function playPause() {
     if (vid.paused) {
         vid.play();
     } else {
         vid.pause();
-        // playbtn.innerHTML = 'Play';
+        // document.getElementById('playPauseBtn').value="x";
     }
 }
 
 function pictureSettings() {
     if (pictureBox.style.display === "none") {
-        if (cameraBox.style.display === "block") {
-            cameraBox.style.display = "none";
-            pictureBox.style.display = "block";
-        }
         pictureBox.style.display = "block";
     } else {
         pictureBox.style.display = "none";
@@ -39,13 +67,22 @@ function pictureSettings() {
 
 function cameraSettings() {
     if (cameraBox.style.display === "none") {
-        if (pictureBox.style.display === "block") {
-            pictureBox.style.display = "none";
             cameraBox.style.display = "block";
-        } else {
-            cameraBox.style.display = "block";
-        }
     } else {
         cameraBox.style.display = "none";
+    }
+}
+
+function changeText() {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coor = "X: " + x + ", Y: " + y;
+    var flag = true;
+    if (flag === true) {
+        flag = false;
+        document.getElementById("textArea1").innerHTML = coor;
+    } else {
+        document.getElementById("textArea2").innerHTML = coor;
+        flag = true;
     }
 }
